99e508b63ddc6abd2cdf8c0b2216721a96bc19fa
Patrick Brennan <brennanpatrick20@gmail.com>
2021-06-12 20:39:29 -0400

    added load and unload to owner utils

--- bot/utils.py
+++ bot/utils.py
@@ -2,6 +2,7 @@ import logging
 
 import discord
 from discord.ext import commands
+from discord.ext.commands.errors import ExtensionAlreadyLoaded, ExtensionNotFound, NoEntryPointError
 
 log = logging.getLogger('bot')
 
@@ -48,3 +49,71 @@ class _OwnerUtils(commands.Cog):
         
         log.info("Reload finished.")
         await msg.edit(content="The following extensions have been reloaded: ```%s```" % ('\n'.join(final_exts)), delete_after=15)
+    
+    @utils.command(description="Load an extension")
+    @commands.is_owner()
+    async def load(self, ctx, *exts):
+
+        if not exts:
+            await ctx.reply("Please specify which extensions to load")
+            return
+
+        msg = await ctx.reply("Loading extensions...")
+        log.info(f'Load called in channel {ctx.channel.id}')
+        
+        if 'all' in [e.lower() for e in exts]:
+            exts = [*self.bot.extensions]
+        
+        final_exts = [*exts]
+
+        for ext in exts:
+            
+            try:
+                self.bot.load_exstension(ext)
+                log.info(f"Loaded extension {ext}")
+            except ExtensionNotFound:
+                log.info(f"{ext} was not found.")
+                final_exts[final_exts.index(ext)] = f"{ext} (Not found)"
+            except ExtensionAlreadyLoaded:
+                log.info(f"{ext} was not found.")
+                final_exts[final_exts.index(ext)] = f"{ext} (Not found)"
+            except:
+                log.info(f"{ext} failed to load.")
+                final_exts[final_exts.index(ext)] = f"{ext} (Failed to load)"
+        
+        log.info("Load finished.")
+        await msg.edit(content="The following extensions have been loaded: ```%s```" % ('\n'.join(final_exts)), delete_after=15)
+
+    @utils.command(description="Unload an extension")
+    @commands.is_owner()
+    async def unload(self, ctx, *exts):
+
+        if not exts:
+            await ctx.reply("Please specify which extensions to unload")
+            return
+
+        msg = await ctx.reply("Unloading extensions...")
+        log.info(f'Unload called in channel {ctx.channel.id}')
+        
+        if 'all' in [e.lower() for e in exts]:
+            exts = [*self.bot.extensions]
+        
+        final_exts = [*exts]
+
+        for ext in exts:
+            
+            try:
+                self.bot.load_exstension(ext)
+                log.info(f"Unloaded extension {ext}")
+            except ExtensionNotFound:
+                log.info(f"{ext} was not found.")
+                final_exts[final_exts.index(ext)] = f"{ext} (Not found)"
+            except ExtensionAlreadyLoaded:
+                log.info(f"{ext} was not found.")
+                final_exts[final_exts.index(ext)] = f"{ext} (Not found)"
+            except:
+                log.info(f"{ext} failed to load.")
+                final_exts[final_exts.index(ext)] = f"{ext} (Failed to unload)"
+        
+        log.info("Unload finished.")
+        await msg.edit(content="The following extensions have been unloaded: ```%s```" % ('\n'.join(final_exts)), delete_after=15)
